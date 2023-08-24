d8ee6aa86d93823f49227bc43e86d3366d6898ad
dbquiroga <day@xcapit.com>
2023-08-24 16:14:15 -0300

    fix products test and change readme

--- README.md
+++ README.md
@@ -50,6 +50,16 @@ npm run cy:report
 
 3.2 Go to the [Dashboard in cypress](https://cloud.cypress.io/projects/frqpqd/)
 
+## Continuous Integration (CI)
+
+This repository has continuous integration set up using GitHub Actions. This means that the automated tests are run automatically whenever changes are pushed to the repository. You can view the results of the test runs in the "Actions" tab above. Feel free to explore the workflows and their execution details.
+
+Additionally, you can also access the artifacts generated during the test runs, which include screenshots and videos captured during the test execution:
+
+- **Screenshots**: Screenshots are automatically generated and stored only if an end-to-end (E2E) test fails. You can find the screenshots in the "Artifacts" section of the specific workflow run, under the "cypress-screenshots" artifact.
+
+- **Videos**: Test run videos are always captured and stored as artifacts. You can access the test run videos in the "Artifacts" section of the workflow run, under the "cypress-videos" artifact.
+
 ## Contact
 
 If you have any questions or have feedback, please don't hesitate to reach out to me at dbquiroga.day@gmail.com.
--- cypress/e2e/addProduct.cy.js
+++ cypress/e2e/addProduct.cy.js
@@ -1,36 +1,32 @@
 import { inventoryPag } from "../support/pages/inventoryPage";
-import { products } from "../fixtures/products.json"
+import { products } from "../fixtures/products.json";
 /// <reference types="cypress" />
 
 describe("Funcionalidad añadir un producto al carrito de compras", () => {
-  
   beforeEach("", () => {
     cy.login();
     cy.url().should("include", "inventory");
   });
-    
+
   it("TC7: Usuario añade un producto del PLP al Shopping-Cart exitosamente.", () => {
     inventoryPag.addProducToCart(products.name);
 
     inventoryPag.get.removeButtonCard().should("contain", "Remove");
-    inventoryPag.get.shoppingCardBadge()
+    inventoryPag.get
+      .shoppingCardBadge()
       .should("exist")
       .should("have.text", "1")
       .click();
     cy.url().should("include", "cart");
 
-    inventoryPag.get.inventoryItemName().should(
-      "contain",
-      products.name
-    );
-    inventoryPag.get.inventoryItemDesc().should(
-      "contain",
-      products.inventoryItemDescription
-    );
-    inventoryPag.get.inventoryItemPrice().should(
-      "contain",
-      products.inventoryItemPrice
-    );
+    inventoryPag.get.inventoryItemName().should("contain", products.name);
+    inventoryPag.get
+      .inventoryItemDesc()
+      .should(
+        "contain",
+        "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection."
+      );
+    inventoryPag.get.inventoryItemPrice().should("contain", "29.99");
   });
   it("TC8: Usuario añade un producto del Product detail Page al Shopping-Cart exitosamente.", () => {
     inventoryPag.seeProduct(products.name);
@@ -38,24 +34,21 @@ describe("Funcionalidad añadir un producto al carrito de compras", () => {
 
     inventoryPag.get.addButtonCard().click();
     inventoryPag.get.removeButtonCard().should("contain", "Remove");
-    inventoryPag.get.shoppingCardBadge()
+    inventoryPag.get
+      .shoppingCardBadge()
       .should("exist")
       .should("have.text", "1")
       .click();
 
-    inventoryPag.get.inventoryItemName().should(
-      "contain",
-      products.name
-    );
-        cy.log(products.itemDescription)
-      
-    inventoryPag.get.inventoryItemDesc().should(
-    "contain",
-      "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection."
-    );
-    inventoryPag.get.inventoryItemPrice().should(
-      "contain",
-     "29.99"
-    );
+    inventoryPag.get.inventoryItemName().should("contain", products.name);
+    cy.log(products.itemDescription);
+
+    inventoryPag.get
+      .inventoryItemDesc()
+      .should(
+        "contain",
+        "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection."
+      );
+    inventoryPag.get.inventoryItemPrice().should("contain", "29.99");
   });
 });
